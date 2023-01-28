33317588074c9871011364f7d23a288771b20959
mayank3005 <saimayank2001@gmail.com>
2023-01-28 23:08:52 +0530

    Project commit 2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('./db/connection');
+const path = require('path');
 
 const User = require('./models/user');
 
@@ -16,12 +17,19 @@ const User = require('./models/user');
 // app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // Middleware
+app.use(express.static(path.join(__dirname, './frontend/build')));
 app.use(express.json());
 app.use(require('./routers/auth')); // link the router files to make routing easy
 
-app.get('/', (req, res) => {
-    res.send('hello from the other side');
-})
+
+app.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, './frontend/build/index.html'),
+        function (err) {
+            res.status(500).send(err);
+        }
+    )
+});
 // app.get('/about', (req, res) => {
 //     res.send('hello from the about page');
 // })
@@ -35,7 +43,7 @@ app.get('/register', (req, res) => {
     res.send('hello from the signup page');
 })
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
